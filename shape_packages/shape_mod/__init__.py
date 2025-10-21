496f1885e4caa673ba1c822a4136aa71a60bec9c
Andrés Felipe Gutiérrez Giraldo <angutierrezgi@unal.edu.co>
2025-10-21 16:10:02 -0500

    Create __init__.py

---
+++ shape_packages/shape_mod/__init__.py
@@ -0,0 +1,11 @@
+from .point import Point
+from .line import Line
+from .shape  import Shape
+from .rectangle import Rectangle, Square
+from .triangle import Triangle, Equilateral, Isosceles, Scalene, TriRectangle
+
+__all__ = [
+    "Point", "Line", "Shape",
+    "Rectangle", "Square",
+    "Triangle", "Equilateral", "Isosceles", "Scalene", "TriRectangle"
+]
