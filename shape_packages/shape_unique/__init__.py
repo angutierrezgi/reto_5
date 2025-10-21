c56e77047ff8fe6c66001c72f258d65d440b3264
Andrés Felipe Gutiérrez Giraldo <angutierrezgi@unal.edu.co>
2025-10-21 16:10:37 -0500

    Create __init__.py

---
+++ shape_packages/shape_unique/__init__.py
@@ -0,0 +1,7 @@
+from .shape import Point, Line, Shape, Rectangle, Square, Triangle, Equilateral, Isosceles, Scalene, TriRectangle
+
+__all__ = [
+    "Point", "Line", "Shape",
+    "Rectangle", "Square",
+    "Triangle", "Equilateral", "Isosceles", "Scalene", "TriRectangle"
+]
